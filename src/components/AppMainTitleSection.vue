048b4f2a7b2f095158365b14fbb749c06a0bd589
vitodursopettersson <vitodurso98@gmail.com>
2023-12-14 11:00:39 +0100

    main title section

---
+++ src/components/AppMainTitleSection.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    name: 'AppMainTitleSection',
+    props: {
+        textSection: String,
+    }
+}
+</script>
+
+<template>
+    <div class="title-container">
+        <h3>{{ textSection }}</h3>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.title-container {
+    width: fit-content;
+    background-color: #0A7CEC;
+    padding: 10px 30px;
+
+    h3 {
+        color: #fff;
+        font-size: 19px;
+        font-weight: 700;
+        text-transform: uppercase;
+    }
+}
+</style>
