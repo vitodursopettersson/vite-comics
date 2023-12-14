47d6c8ccc5b1e74a763072e5f9e15388f936d060
vitodursopettersson <vitodurso98@gmail.com>
2023-12-14 09:51:33 +0100

    jumbotron

---
+++ src/components/AppHeaderJumbotron.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: 'AppHeaderJumbotron',
+}
+</script>
+
+<template>
+    <div class="jumbotron">
+        <img src="../assets/img/jumbotron.jpg" alt="jumbotron image">
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.jumbotron {
+    width: 100%;
+    height: 400px;
+
+    img {
+        width: 100%;
+        height: 100%;
+        display: block;
+        object-fit: cover;
+        object-position: top;
+    }
+
+}
+</style>
